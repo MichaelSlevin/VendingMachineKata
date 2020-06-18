27fd3ae9f20050c1f71c2c6397b9d62802745724
Michael SLEVIN <Michael.SLEVIN@EDUCATION.GOV.UK>
2020-06-19 00:36:46 +0100

    Test written for money handler class but not compiling

---
+++ VendingMachineTests/MoneyHandlerTest.cs
@@ -0,0 +1,30 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VendingMachineKata;
+using Moq;
+
+namespace VendingMachineTests
+{
+    [TestClass]
+    public class MoneyHandlerTests
+    {
+        
+        [TestMethod]
+        public void MoneyHandler_InsertCoin_AddsCoinToTheStoredCoinsList()
+        {
+            var moneyHandler = new MoneyHandler();
+            Assert.AreEqual(0, moneyHandler.StoredCoins.Count);
+
+            var mockCoin = new Mock<Coin>();
+
+            moneyHandler.InsertCoin(mockCoin);
+
+            Assert.AreEqual(0, moneyHandler.StoredCoins.Count);
+            Assert.IsTrue(moneyHandler.Contains(mockCoin));
+
+
+
+        }
+
+      
+    }
+}
