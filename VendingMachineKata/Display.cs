36ce15287f1c71d656843c680407b8410f0dc8e4
Michael SLEVIN <Michael.SLEVIN@EDUCATION.GOV.UK>
2020-06-22 07:59:54 +0100

    insert coin message test passing

---
+++ VendingMachineKata/Display.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VendingMachineKata
+{
+    public class Display
+    { 
+        public Display()
+        {
+        }
+
+        public string InsertCoinMessage()
+        {
+            return "INSERT COIN";
+        }
+    }
+}
