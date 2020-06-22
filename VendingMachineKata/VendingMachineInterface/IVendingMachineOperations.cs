b8b4c56f10178f2a6512661ab23c20c24c1241ca
Michael SLEVIN <Michael.SLEVIN@EDUCATION.GOV.UK>
2020-06-22 21:19:29 +0100

    added an empty IVendingMachineOperations interface

---
+++ VendingMachineKata/VendingMachineInterface/IVendingMachineOperations.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VendingMachineKata.VendingMachineInterface
+{
+    //this interface remains unimplemented however it defines the contract that that objects responsible for
+    //operating the vending machine should implement
+    public interface IVendingMachineOperations
+    {
+
+    }
+}
