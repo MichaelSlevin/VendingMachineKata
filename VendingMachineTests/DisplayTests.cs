bd603ec0c1a68f5e6f08590a68b2cded357a659b
Michael SLEVIN <Michael.SLEVIN@EDUCATION.GOV.UK>
2020-06-22 18:29:34 +0100

    tests added for thank you messafe and insufficient funds message on DisplayTests

--- VendingMachineTests/DisplayTests.cs
+++ VendingMachineTests/DisplayTests.cs
@@ -14,6 +14,25 @@ namespace VendingMachineTests
             Assert.AreEqual("INSERT COIN", display.InsertCoinMessage());
         }
 
+        [TestMethod]
+        public void Display_ThankYouMessage()
+        {
+            var display = new Display();
+            Assert.AreEqual("THANK YOU", display.ThankYouMessage());
+        }
+
+        [DataTestMethod]
+        [DataRow(100, "PRICE $1.00")]
+        [DataRow(50, "PRICE $0.50")]
+        [DataRow(65, "PRICE $0.65")]
+        public void Display_InsufficientFunds(int priceOfItem, string expectedResult)
+        {
+            var display = new Display();
+            Assert.AreEqual(expectedResult, display.InsufficientFunds(priceOfItem));
+        }
+
+
+
 
     }
 }
