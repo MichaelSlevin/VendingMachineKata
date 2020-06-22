9f006390431df4f93e128b304ebcf4adf5c14ebe
Michael SLEVIN <Michael.SLEVIN@EDUCATION.GOV.UK>
2020-06-22 19:42:28 +0100

    product constructor tests passing

--- VendingMachineKata/Model/Product.cs
+++ VendingMachineKata/Model/Product.cs
@@ -7,6 +7,28 @@ namespace VendingMachineKata.Model
         public Product()
         {
         }
-       
+
+        public Product(string name)
+        {
+            this.Name = name;
+            switch (name) 
+            {
+                case "cola":
+                    this.Cost = 100;                   
+                    break;
+                case "candy":
+                    this.Cost = 65;                   
+                    break;
+                case "chips":
+                    this.Cost = 50;                   
+                    break;
+            }
+
+        }
+
+        public string Name { get; private set; }
+
+        public int Cost { get; private set; }
+
     }
 }
