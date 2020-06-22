f07c4451024ee7442a915e237629c3240a77f3e7
Michael SLEVIN <Michael.SLEVIN@EDUCATION.GOV.UK>
2020-06-22 07:57:05 +0100

    insert coin message written and failing

--- VendingMachineTests/DisplayTests.cs
+++ VendingMachineTests/DisplayTests.cs
@@ -7,7 +7,13 @@ namespace VendingMachineTests
     [TestClass]
     public class DisplayTests
     {
-        
-    
+        [TestMethod]
+        public void Display_InsertCoinMessage()
+        {
+            var display = new Display();
+            Assert.AreEqual("INSERT COIN", display.InsertCoinMessage());
+        }
+
+
     }
 }
