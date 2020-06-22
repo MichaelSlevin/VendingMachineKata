85e883a18d3777b1f8bf4b80cdb042d6cd72d517
Michael SLEVIN <Michael.SLEVIN@EDUCATION.GOV.UK>
2020-06-22 22:43:28 +0100

    Updated money handler tests to derive current balance and separate out the currently inserted coins and the stored coins

--- VendingMachineTests/Services/MoneyHandlerTest.cs
+++ VendingMachineTests/Services/MoneyHandlerTest.cs
@@ -9,20 +9,29 @@ namespace VendingMachineTests.Services
     [TestClass]
     public class MoneyHandlerTests
     {
+
+        public MoneyHandlerTests()
+        {
+            var dime1 = new Coin(Constants.WeightOfDime, Constants.DiameterOfDime);
+            var dime2 = new Coin(Constants.WeightOfDime, Constants.DiameterOfDime);
+            var nickel1 = new Coin(Constants.WeightOfNickel, Constants.DiameterOfNickel);
+        }
         
         [TestMethod]
         public void MoneyHandler_InsertCoin_AddsCoinToTheStoredCoinsList()
         {
             var moneyHandler = new MoneyHandler();
-            Assert.AreEqual(0, moneyHandler.StoredCoins.Count);
+            Assert.AreEqual(0, moneyHandler.InsertedCoins.Count);
 
             var coin = new Coin(Constants.WeightOfDime, Constants.DiameterOfDime);
 
             moneyHandler.InsertCoin(coin);
 
-            Assert.AreEqual(1, moneyHandler.StoredCoins.Count);
-            Assert.IsTrue(moneyHandler.StoredCoins.Contains(coin));
+            Assert.AreEqual(1, moneyHandler.InsertedCoins.Count);
+            Assert.IsTrue(moneyHandler.InsertedCoins.Contains(coin));
         }
+
+        
 
         [TestMethod]
         public void MoneyHandler_InsertCoin_AddsCoinToReturnedCoinsWhenInvalid()
@@ -34,18 +43,22 @@ namespace VendingMachineTests.Services
 
             moneyHandler.InsertCoin(coin);
 
-            Assert.AreEqual(0, moneyHandler.StoredCoins.Count);        
+            Assert.AreEqual(0, moneyHandler.InsertedCoins.Count);        
             Assert.AreEqual(1, moneyHandler.ReturnedCoins.Count);        
         }
 
         [TestMethod]
-        public void MoneyHandler_InsertCoin_UpdatesCurrentAmountWhenValid()
+        public void MoneyHandler_GetCurrentBalance_CorrectlyReturnsTheCurrentBalance()
         {
+
+
             var moneyHandler = new MoneyHandler();
-            Assert.AreEqual(0, moneyHandler.CurrentAmount);
-            var dime = new Coin(Constants.WeightOfDime, Constants.DiameterOfDime);
-            moneyHandler.InsertCoin(dime);
-            Assert.AreEqual(10, moneyHandler.CurrentAmount);
+            Assert.AreEqual(0, moneyHandler.GetCurrentBalance());
+            moneyHandler.InsertedCoins.Add(dime1);
+            moneyHandler.InsertedCoins.Add(dime2);
+            moneyHandler.InsertedCoins.Add(nickel1);
+
+            Assert.AreEqual(25, moneyHandler.GetCurrentBalance()) ;
         }
 
         [TestMethod]
@@ -101,6 +114,27 @@ namespace VendingMachineTests.Services
             Assert.AreEqual("Not accepted", notAcceptedResult.Name);
 
         }
+
+
+        [TestMethod]
+        public void MoneyHandler_CompleteSale_MovesInsertedCoinsToStoredCoinsList()
+        {
+            var moneyHandler = new MoneyHandler();
+            moneyHandler.InsertedCoins.Add(dime1);
+            moneyHandler.InsertedCoins.Add(nickel1);
+
+            Assert.AreEqual(0, moneyHandler.StoredCoins);
+            Assert.AreEqual(2, moneyHandler.InsertedCoins);
+            moneyHandler.CompleteSale();
+
+            Assert.AreEqual(2, moneyHandler.StoredCoins);
+            Assert.AreEqual(0, moneyHandler.InsertedCoins);
+
+            Assert.IsTrue(moneyHandler.StoredCoins.Contains(dime1));
+            Assert.IsTrue(moneyHandler.StoredCoins.Contains(nickel1));
+        }
+
+
 
 
     }
