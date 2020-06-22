5e573b3b6d406de70ea836dae239a7acde41577d
Michael SLEVIN <Michael.SLEVIN@EDUCATION.GOV.UK>
2020-06-22 08:22:34 +0100

    Get product price by name passing

---
+++ VendingMachineKata/ProductHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VendingMachineKata
+{
+    public class ProductHandler
+    {
+        public ProductHandler()
+        {
+        }
+
+        public int GetProductPriceByName(string name)
+        {
+            if (name == "cola")
+                return 100;
+            if (name == "chips")
+                return 50;
+            if (name == "candy")
+                return 65;
+            else
+                return 0;
+        }
+    }
+}
+
